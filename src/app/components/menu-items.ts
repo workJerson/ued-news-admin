ba15ab4ab5de574fa0af516276f3c33119a2c3cf
jerson <jerson.baltazar@myxlog.com>
2021-07-22 10:56:00 +0800

    Added icons in menu

--- src/app/components/menu-items.ts
+++ src/app/components/menu-items.ts
@@ -2,7 +2,7 @@ import { MenuItem } from '../models/menu-item.model';
 
 export const MENU_ITEMS: MenuItem[] = [
   {
-    icon: 'NA',
+    icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01',
     name: 'Dashboard',
     isActive: false,
     route: '/main/dashboard',
@@ -22,35 +22,35 @@ export const MENU_ITEMS: MenuItem[] = [
     ],
   },
   {
-    icon: 'NA',
+    icon: 'M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z',
     name: 'Articles',
     isActive: false,
     route: '/main/articles',
     children: [],
   },
   {
-    icon: 'NA',
+    icon: 'M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10',
     name: 'Categories',
     isActive: false,
     route: '/main/article-categories',
     children: [],
   },
   {
-    icon: 'NA',
+    icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
     name: 'Tags',
     isActive: false,
     route: '/main/tags',
     children: [],
   },
   {
-    icon: 'NA',
+    icon: 'M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z',
     name: 'Users',
     isActive: false,
     route: '/main/users',
     children: [],
   },
   {
-    icon: 'NA',
+    icon: 'M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z',
     name: 'Settings',
     isActive: false,
     route: '/main/settings',
