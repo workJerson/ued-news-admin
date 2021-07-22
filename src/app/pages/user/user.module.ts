a482c91b3d047d8c83db31262f128efa8fbfd2e5
jerson <jerson.baltazar@myxlog.com>
2021-07-22 20:06:05 +0800

    Added component for create & update user

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UpdateUserComponent, CreateUserComponent],
   imports: [
     CommonModule,
     UserRoutingModule
